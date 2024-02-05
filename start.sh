40793cdc4026edcacb85b3368ed59e28bbe55bf6
Fellarrusto <filippo.leonelli@linksmt.it>
2024-02-05 16:49:39 +0100

    Updated start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # start.sh
 
-echo "Migrations..."
-python manage.py migrate --noinput
+# echo "Migrations..."
+# python manage.py migrate --noinput
 
 echo "Starting server..."
 gunicorn eVDRBackend.wsgi:application --bind 0.0.0.0:8000
